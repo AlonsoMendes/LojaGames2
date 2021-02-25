866fad63a4822e5f0d4aa0f3274b7dbf52457f3c
AMS <alonso_mendess@mail.com>
2021-02-25 18:11:42 -0300

    Criação da Model/ Controller / Respository do usuario

--- LojaDeGames/src/main/java/org/generation/LojaDeGames/repository/UsuarioRepository.java
+++ LojaDeGames/src/main/java/org/generation/LojaDeGames/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package org.generation.LojaDeGames.repository;
 
-public class UsuarioRepository {
+import java.util.List;
 
+import org.generation.LojaDeGames.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	public List<UsuarioRepository> findAllByNomeCompletoContainingIgnoreCase( String nomeCompleto);
 }
